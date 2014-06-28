62ecbf95ad5616016d9821bda047380194990aeb
vikasamin <vikasamin@rediffmail.com>
2014-06-28 13:47:51 -0400

    Initial Commit for Windows Phone

---
+++ client/TransitApp.WP/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using Cirrious.MvvmCross.WindowsPhone.Views;
+
+namespace TransitApp.WP.Views
+{
+    public partial class FirstView : MvxPhonePage
+    {
+        public FirstView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ client/TransitApp.WP/Views/HomeView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Cirrious.MvvmCross.WindowsPhone.Views;
+
+namespace TransitApp.WP.Views
+{
+    public partial class HomeView : MvxPhonePage
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
