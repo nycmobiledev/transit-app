d6d0803724053354127bffbca40ea640fb73bb53
Jason <jasonvarny@users.noreply.github.com>
2014-06-30 01:01:33 -0400

    initial .NET project shell for Azure Mobile Service

---
+++ service/Azure_Mobile_Services/transit-app/transit-app/App_Start/WebApiConfig.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Web.Http;
+using transit_app.DataObjects;
+using transit_app.Models;
+using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace transit_app
+{
+    public static class WebApiConfig
+    {
+        public static void Register()
+        {
+            // Use this class to set configuration options for your mobile service
+            ConfigOptions options = new ConfigOptions();
+
+            // Use this class to set WebAPI configuration options
+            HttpConfiguration config = ServiceConfig.Initialize(new ConfigBuilder(options));
+
+            // To display errors in the browser during development, uncomment the following
+            // line. Comment it out again when you deploy your service for production use.
+            // config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+
+            Database.SetInitializer(new transit_appInitializer());
+        }
+    }
+
+    public class transit_appInitializer : DropCreateDatabaseIfModelChanges<transit_appContext>
+    {
+        protected override void Seed(transit_appContext context)
+        {
+            List<TodoItem> todoItems = new List<TodoItem>
+            {
+                new TodoItem { Id = "1", Text = "First item", Complete = false },
+                new TodoItem { Id = "2", Text = "Second item", Complete = false },
+            };
+
+            foreach (TodoItem todoItem in todoItems)
+            {
+                context.Set<TodoItem>().Add(todoItem);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
+
---
+++ service/Azure_Mobile_Services/transit-app/transit-app/DataObjects/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace transit_app.DataObjects
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
