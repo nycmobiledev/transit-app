98ffcf5059a633999d734abd6f59b6c3668c307b
Vincent Bortone <vbortone@gmail.com>
2014-07-14 08:25:40 -0400

    Rename Test file

---
+++ service/TransitApp.Server/TransitApp.Server.IntegrationTests/FeedMessageServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TransitApp.Server.GTFSRealtime;
+using TransitApp.Server.GTFSRealtime.Entities;
+
+namespace TransitApp.Server.IntegrationTests
+{
+    [TestFixture]
+    public class FeedMessageServiceTests
+    {
+        private const string apiKey = "80730fbe1b42c61fc060da055cb33334";
+        private const string url = "http://datamine.mta.info/mta_esi.php?key={0}&feed_id=";
+        private string combinedUrl;
+
+        [TestFixtureSetUp]
+        public void SetupFixture()
+        {
+            combinedUrl = string.Format(url, apiKey);
+        }
+    }
+}
