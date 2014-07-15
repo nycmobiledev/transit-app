8bfa1d84977eeee91f8d99e25b53bd5b13ffdc08
Vincent Bortone <vbortone@gmail.com>
2014-07-15 02:37:20 -0400

    Added DataLoader Project

--- service/TransitApp.Server/GTFSRealtime.DataLoader/Program.cs
+++ service/TransitApp.Server/GTFSRealtime.DataLoader/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using TransitApp.Server.GTFSRealtime.Core.Model;
+using TransitApp.Server.GTFSRealtime.Core.Services;
+using TransitApp.Server.GTFSRealtime.Infrastructure.MTA;
 
 namespace GTFSRealtime.DataLoader
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        protected const string ApiKey = "80730fbe1b42c61fc060da055cb33334";
+        protected const string Url = "http://datamine.mta.info/mta_esi.php?key={0}&feed_id=";
+
+        private static void Main(string[] args)
         {
+            AsyncMain().Wait();
+        }
+
+        private static async Task AsyncMain()
+        {
+            // Load Feed Message
+            var combinedUrl = string.Format(Url, ApiKey);
+            var service = new FeedMessageService(combinedUrl);
+            var msgL = await service.GetCurrentRealtimeFeedMessage(SubwayLines.L);
+            var msgIrt = await service.GetCurrentRealtimeFeedMessage(SubwayLines.RED_GREEN_S);
+
+            var alertFactory = new AlertFactory();
+            var alertsIrt = alertFactory.CreateItemsFromFeedMessage(msgIrt);
+            var alertsL = alertFactory.CreateItemsFromFeedMessage(msgL);
+
+            foreach (var alertIrt in alertsIrt)
+            {
+                Console.WriteLine("{0}: {1}", alertIrt.TripId, alertIrt.Message);
+            }
+
+            foreach (var alertL in alertsL)
+            {
+                Console.WriteLine("{0}: {1}", alertL.TripId, alertL.Message);
+            }
+
+            // Clear Tables
+
+            // Load Tables
         }
     }
 }
