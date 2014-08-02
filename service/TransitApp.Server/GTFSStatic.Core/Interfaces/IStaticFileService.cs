5a6db05b74d15b2a0441a7af3a1508fc5d84aba3
Vincent Bortone <vbortone@gmail.com>
2014-08-02 05:34:44 -0400

    Start GFTSStatic projects

---
+++ service/TransitApp.Server/GTFSStatic.Core/Interfaces/IStaticFileService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using TransitApp.Server.GTFSStatic.Core.Model;
+
+namespace TransitApp.Server.GTFSStatic.Core.Interfaces
+{
+    public interface IStaticFileService
+    {
+        IList<Agency> GetAgencies();
+    }
+}
---
+++ service/TransitApp.Server/GTFSStatic.Core/Model/Agency.cs
@@ -0,0 +1,12 @@
+namespace TransitApp.Server.GTFSStatic.Core.Model
+{
+    public class Agency
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Timezone { get; set; }
+        public string Language { get; set; }
+        public string Phone { get; set; }
+    }
+}
