025c638ea51884e4f5c938584c96bb1e400aa93a
Vincent Bortone <vbortone@gmail.com>
2014-08-11 07:42:26 -0400

    Update Core project interfaces

---
+++ service/TransitApp.Server/GTFSStatic.Core/Interfaces/IStaticFileDownloader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO.Compression;
+using System.Threading.Tasks;
+
+namespace TransitApp.Server.GTFSStatic.Core.Interfaces
+{
+    public interface IStaticFileDownloader
+    {
+        Task<ZipArchive> DownloadZipFileFromUrl(string url);
+    }
+}
--- service/TransitApp.Server/GTFSStatic.Core/Interfaces/IStaticFileService.cs
+++ service/TransitApp.Server/GTFSStatic.Core/Interfaces/IStaticFileService.cs
@@ -7,5 +7,13 @@ namespace TransitApp.Server.GTFSStatic.Core.Interfaces
     public interface IStaticFileService
     {
         IList<Agency> GetAgencies();
+        IList<Calendar> GetCalendars();
+        IList<CalendarDate> GetCalendarDates();
+        IList<Route> GetRoutes();
+        IList<Shape> GetShapes();
+        IList<Stop> GetStops();
+        IList<StopTime> GetStopTimes();
+        IList<Transfer> GetTransfers();
+        IList<Trip> GetTrips();
     }
 }
