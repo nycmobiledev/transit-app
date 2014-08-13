78a6e94cf68dc02854871a7287c1604efbd1ca47
Vincent Bortone <vbortone@gmail.com>
2014-08-13 13:51:51 -0400

    Add Test Project

---
+++ service/TransitApp.Server/GTFSStatic.InfrastructureTests/StaticFileDownloaderTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using TransitApp.Server.GTFSStatic.Infrastructure.MTA;
+
+namespace TransitApp.Server.GTFSStatic.InfrastructureTests
+{
+    [TestFixture]
+    internal class StaticFileDownloaderTests
+    {
+        [Test]
+        public async void Should_Return_ZipArchive()
+        {
+            var downloader = new StaticFileDownloader();
+            var zipfile =
+                await
+                    downloader.DownloadZipFileFromUrl(
+                        "http://web.mta.info/developers/data/nyct/subway/google_transit.zip");
+            Assert.That(zipfile.Entries.Count, Is.GreaterThan(1));
+        }
+    }
+}
