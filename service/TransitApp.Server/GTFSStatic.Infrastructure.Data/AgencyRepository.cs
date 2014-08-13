3a4919523e469f7b47d29df9d28e63de89afa8eb
Vincent Bortone <vbortone@gmail.com>
2014-08-13 13:47:27 -0400

    Implement AgencyRepository

---
+++ service/TransitApp.Server/GTFSStatic.Infrastructure.Data/AgencyRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TransitApp.Server.GTFSStatic.Core.Model;
+using TransitApp.Server.Shared.Core.Interfaces;
+using TransitApp.Server.Shared.Infrastructure.Data;
+
+namespace TransitApp.Server.GTFSStatic.Infrastructure.Data
+{
+    internal class AgencyRepository : RepositoryBase<Agency>, IGTFSRepository<Agency>
+    {
+        public AgencyRepository(string connectionString)
+            : base(connectionString, "dbo.agency",
+                new[] {
+                    new ColumnMapping("agency_id", typeof (string)),
+                    new ColumnMapping("agency_name", typeof (string)),
+                    new ColumnMapping("agency_url", typeof (string)),
+                    new ColumnMapping("agency_timezone", typeof (string)),
+                    new ColumnMapping("agency_lang", typeof (string)),
+                    new ColumnMapping("agency_phone", typeof (string))
+                }) {}
+
+        public void AddRange(IEnumerable<Agency> items)
+        {
+            SqlBulkInsertTable(items);
+        }
+
+        public void ClearAll()
+        {
+            PurgeTable();
+        }
+
+        public override void CreateDataTableFromItems(IEnumerable<Agency> items)
+        {
+            var agencies = items as IList<Agency> ?? items.ToList();
+            base.CreateDataTableFromItems(agencies);
+
+            foreach (var item in agencies) {
+                InsertDataTable.Rows.Add(item.Id, item.Name, item.Url, item.Timezone, item.Language, item.Phone);
+            }
+        }
+    }
+}
