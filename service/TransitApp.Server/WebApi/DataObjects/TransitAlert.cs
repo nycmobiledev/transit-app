bb1d58e8f3a105c91366b1a8577df2807e6771ab
maheedev <nalmans@hotmail.com>
2014-08-13 21:07:39 -0400

    Azure Mobile Service

---
+++ service/TransitApp.Server/WebApi/DataObjects/TransitAlert.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.DataObjects
+{
+    public class TransitAlert : EntityData
+    {
+        public string Route { get; set; }
+
+        public string Time { get; set; }
+    }
+}
