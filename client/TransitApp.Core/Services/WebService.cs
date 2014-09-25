62c4218db77b23e5f37d4788449a8d53dd3f0d11
Somya Jain <somya.jain@cbsinteractive.com>
2014-09-24 20:27:48 -0400

    Added client side calls for service.

--- client/TransitApp.Core/Services/WebService.cs
+++ client/TransitApp.Core/Services/WebService.cs
@@ -18,16 +18,16 @@ namespace TransitApp.Core.Services
 		public double Price {get;set;}
 		public int PortfolioQty {get;set;}
 	}
-    public class WebService : IWebService
-    {
+	public class WebService : IWebService
+	{
 		private CoolTimer  _coolTimer ;
 
 
-        public WebService()
-        {
+		public WebService()
+		{
 			_coolTimer = new CoolTimer (TimerCallback,null,0,1000);
 
-        }
+		}
 
 		void TimerCallback(object state)
 		{
@@ -35,12 +35,12 @@ namespace TransitApp.Core.Services
 		}
 
 		public async Task<ICollection<Station>> FindStationsByName(string name)
-        {
-            //todo 
-            throw new NotImplementedException();
-        }
-        public async Task<ICollection<Alert>> GetAlerts(IEnumerable<Follow> follows)
-        {
+		{
+			//todo 
+			throw new NotImplementedException();
+		}
+		public async Task<ICollection<Alert>> GetAlerts(IEnumerable<Follow> follows)
+		{
 			/*
 			HttpClient client = new HttpClient ();
 			string json = ""; //await client.GetStringAsync ("http://192.168.2.6:81/transitapp_svc/tables/Train");
@@ -48,24 +48,41 @@ namespace TransitApp.Core.Services
 			string resp = await client.GetStringAsync (url); 
 			var resp1 =  JsonConvert.DeserializeObject<List<TickerModel>>(resp);
 			*/
-            //todo 
+			//todo 
 			//var resp1 = JsonConvert.DeserializeObject<ICollection<Train>> (json);
 
 			var list = new List<Alert>();
 
-			foreach (var follow in follows)
-			{
-				list.Add(new Alert()
-					{
-						TrainId = "1",
-						ArriveTime = DateTime.Now,
-						Station = new Station{Name = "union sq"},
-						Line = new Line{ Id = "1"}
-					});
-			}
+//			foreach (var follow in follows)
+//			{
+//				list.Add(new Alert()
+//					{
+//						TrainId = "1",
+//						ArriveTime = DateTime.Now,
+//						Station = new Station{Name = "union sq"},
+//						Line = new Line{ Id = "1"}
+//					});
+//			}
+
+
+//              MobileServiceClient client = new MobileServiceClient(
+//                "http://transitapp.azure-mobile.net/",
+//                "HaFafTMWBtEycDgGAgJDvlPKibkQIK93"
+//            );
+
+			HttpClient client = new HttpClient();
+
+
+
+			client.DefaultRequestHeaders.Add("X-ZUMO-APPLICATION", "HaFafTMWBtEycDgGAgJDvlPKibkQIK93");
+
+			var resp = await client.GetStringAsync("http://transitapp.azure-mobile.net/api/TransitAlert?stationsCsv=640");
+				  
+			list = JsonConvert.DeserializeObject<List<Alert>>(resp);
+
 
 			return list;
 
-        }
-    }
+		}
+	}
 }
