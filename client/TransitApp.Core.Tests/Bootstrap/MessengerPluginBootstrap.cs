7b737e804271de597573458059c4e8934e42b4fa
Wade <wadehuang36@gmail.com>
2014-09-24 20:49:09 -0400

    MvvxCross Packages upgrades

---
+++ client/TransitApp.Core.Tests/Bootstrap/MessengerPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace TransitApp.Core.Tests.Bootstrap
+{
+    public class MessengerPluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Messenger.PluginLoader>
+    {
+    }
+}
---
+++ client/TransitApp.Droid/Bootstrap/FilePluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace TransitApp.Droid.Bootstrap
+{
+    public class FilePluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.File.PluginLoader>
+    {
+    }
+}
