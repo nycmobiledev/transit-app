2e957038d98ca9fa41a55e810a55202aa1bf7cfd
Somya Jain <somya.jain@cbsinteractive.com>
2014-10-26 11:16:14 -0400

    Added CreateStaticScheduleJob

---
+++ service/TransitApp.Server/WebApi/ScheduledJobs/CreateStaticScheduleJob.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Mobile.Service;
+using TransitApp.Server.GTFSStatic.Infrastructure.Data;
+using TransitApp.Server.GTFSStatic.Infrastructure.MTA;
+
+namespace TransitApp.Server.WebApi.ScheduledJobs
+{
+    public class CreateStaticScheduleJob : ScheduledJob
+    {
+        /// <summary>
+        /// When implemented in a derived class, executes the scheduled job asynchronously. Implementations that want to know whether
+        ///             the scheduled job is being cancelled can get a <see cref="P:Microsoft.WindowsAzure.Mobile.Service.ScheduledJob.CancellationToken"/> from the <see cref="M:CancellationToken"/> property.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.Threading.Tasks.Task"/> representing the asynchronous operation.
+        /// </returns>
+        public override async Task ExecuteAsync()
+        {
+            // Load Feed Message
+            var dbConnStr = ConfigurationManager.ConnectionStrings["MTA_DB"].ConnectionString;
+
+            using (var conn = new SqlConnection(dbConnStr))
+            {
+                conn.Open();
+
+               
+                using (var cmd = new SqlCommand("sp_CreateStaticSchedule", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
