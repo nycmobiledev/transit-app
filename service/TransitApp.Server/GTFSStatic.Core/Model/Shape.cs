346fbfe3960ed2df0897d50439e7974632b98bbd
Wade <wadehuang36@gmail.com>
2014-12-05 16:06:37 -0500

    Match classes to GTFS files

--- service/TransitApp.Server/GTFSStatic.Core/Model/Shape.cs
+++ service/TransitApp.Server/GTFSStatic.Core/Model/Shape.cs
@@ -6,15 +6,18 @@ namespace TransitApp.Server.GTFSStatic.Core.Model
     public class Shape
     {
         [CsvColumn(FieldIndex = 1, Name = "shape_id")]
-        public string ShapeId { get; set; }
+        public string ShapeId { get; set; }       
 
-        [CsvColumn(FieldIndex = 2, Name = "shape_pt_sequence")]
-        public int Sequence { get; set; }
-
-        [CsvColumn(FieldIndex = 3, Name = "shape_pt_lat")]
+        [CsvColumn(FieldIndex = 2, Name = "shape_pt_lat")]
         public double Latitude { get; set; }
 
-        [CsvColumn(FieldIndex = 4, Name = "shape_pt_lon")]
+        [CsvColumn(FieldIndex = 3, Name = "shape_pt_lon")]
         public double Longitude { get; set; }
+
+        [CsvColumn(FieldIndex = 4, Name = "shape_pt_sequence")]
+        public int Sequence { get; set; }
+
+        [CsvColumn(FieldIndex = 5, Name = "shape_dist_traveled")]
+        public string DistTraveled { get; set; }
     }
 }
--- service/TransitApp.Server/GTFSStatic.Core/Model/Stop.cs
+++ service/TransitApp.Server/GTFSStatic.Core/Model/Stop.cs
@@ -8,19 +8,31 @@ namespace TransitApp.Server.GTFSStatic.Core.Model
         [CsvColumn(FieldIndex = 1, Name = "stop_id")]
         public string StopId { get; set; }
 
-        [CsvColumn(FieldIndex = 2, Name = "stop_name")]
+        [CsvColumn(FieldIndex = 2, Name = "stop_code")]
+        public string StopCode { get; set; }
+
+        [CsvColumn(FieldIndex = 3, Name = "stop_name")]
         public string Name { get; set; }
 
-        [CsvColumn(FieldIndex = 3, Name = "stop_lat")]
+        [CsvColumn(FieldIndex = 4, Name = "stop_desc")]
+        public string Desc { get; set; }
+
+        [CsvColumn(FieldIndex = 5, Name = "stop_lat")]
         public double Latitude { get; set; }
 
-        [CsvColumn(FieldIndex = 4, Name = "stop_lon")]
+        [CsvColumn(FieldIndex = 6, Name = "stop_lon")]
         public double Longitude { get; set; }
 
-        [CsvColumn(FieldIndex = 5, Name = "location_type")]
+        [CsvColumn(FieldIndex = 7, Name = "zone_id")]
+        public string ZoneId { get; set; }
+
+        [CsvColumn(FieldIndex = 8, Name = "stop_url")]
+        public string StopUrl { get; set; }
+        
+        [CsvColumn(FieldIndex = 9, Name = "location_type")]
         public int LocationType { get; set; }
 
-        [CsvColumn(FieldIndex = 6, Name = "parent_station")]
+        [CsvColumn(FieldIndex = 10, Name = "parent_station")]
         public string ParentId { get; set; }
     }
 }
